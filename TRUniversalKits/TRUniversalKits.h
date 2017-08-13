ab0e597e4fec887582de59a88476ab5a9fad7d6c
xuyang.bxy <xuyang.bxy@alibaba-inc.com>
2017-08-13 15:12:56 +0800

    Initial Commit

---
+++ TRUniversalKits/TRUniversalKits.h
@@ -0,0 +1,19 @@
+//
+//  TRUniversalKits.h
+//  TRUniversalKits
+//
+//  Created by xuyang on 2017/8/13.
+//  Copyright © 2017年 travel. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for TRUniversalKits.
+FOUNDATION_EXPORT double TRUniversalKitsVersionNumber;
+
+//! Project version string for TRUniversalKits.
+FOUNDATION_EXPORT const unsigned char TRUniversalKitsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TRUniversalKits/PublicHeader.h>
+
+
